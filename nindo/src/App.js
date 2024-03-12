7f0e8a7fd7cd59e6af2e7ef2edd6b4e4b25aa04e
Mohammad Rahal <rahalmohamad21@gmail.com>
2024-03-12 13:11:53 +0200

    making all components and styling it

--- nindo/src/App.js
+++ nindo/src/App.js
@@ -1,9 +1,14 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";import './App.css';
+import HomePage from './pages/HomePage';
 
 function App() {
   return (
     <div>
-     
+    <Router>
+    <Routes>
+    <Route path="/" element={<HomePage />} />
+    </Routes>
+    </Router>
     </div>
   );
 }
---
+++ nindo/src/pages/HomePage.jsx
@@ -0,0 +1,14 @@
+import Header from '../components/Header'
+import Banner from '../components/Banner'
+import Footer from '../components/Footer'
+const HomePaghe = () => {
+  return (
+    <div>
+      <Header/>
+      <Banner/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default HomePaghe
